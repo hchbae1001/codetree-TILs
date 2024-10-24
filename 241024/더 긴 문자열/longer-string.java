1fe35cd528fbd019da918f413897a248b93bb8cb
hchbae <hchbae1001@gmail.com>
2024-10-24 21:59:16 +0900

    Title: 더 긴 문자열, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/더 긴 문자열/longer-string.java
+++ 241024/더 긴 문자열/longer-string.java
@@ -3,19 +3,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] arg = sc.nextLine().split(" ");
-        Map<Integer,String> tmp = new HashMap<>();
-        int max = 0;
-        for(String i : arg){
-            int len = i.length();
-            tmp.put(len, i);
-            if(max < len){
-                max = len;
-            }else if (max == len){
-                System.out.println("same");
-                return;
-            }
+        String[] words = sc.nextLine().split(" ");
+        
+        String word1 = words[0];
+        String word2 = words[1];
+        
+        int len1 = word1.length();
+        int len2 = word2.length();
+        
+        if (len1 > len2) {
+            System.out.println(word1 + " " + len1);
+        } else if (len1 < len2) {
+            System.out.println(word2 + " " + len2);
+        } else {
+            System.out.println("same");
         }
-        System.out.print(tmp.get(max)+ " " + max);
     }
 }
