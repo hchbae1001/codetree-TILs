4a86e0ed730a8a035c714dd49d2e5c5ed7ed1ddf
hchbae <hchbae1001@gmail.com>
2024-10-24 23:04:21 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
+++ 241024/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] arg = new String[10];
+        Boolean noAnswer = true;
         for(int i = 0; i < 10; i++){
             arg[i] = sc.nextLine();
         }
@@ -11,8 +12,11 @@ public class Main {
         for(String tmp : arg){
             if(tmp.charAt(tmp.length()-1) == target){
                 System.out.println(tmp);
+                noAnswer = false;
             }
         }
-
+        if(noAnswer){
+            System.out.println("None");
+        }
     }
 }
