f65a94f42b71061fae2e462d155d69081647e0fb
hchbae <hchbae1001@gmail.com>
2024-10-24 21:57:04 +0900

    Title: 더 긴 문자열, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241024/더 긴 문자열/longer-string.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arg = sc.nextLine().split(" ");
+        Map<Integer,String> tmp = new HashMap<>();
+        int max = 0;
+        for(String i : arg){
+            int len = i.length();
+            tmp.put(len, i);
+            if(max < len){
+                max = len;
+            }
+        }
+        System.out.print(tmp.get(max)+ " " + max);
+    }
+}
