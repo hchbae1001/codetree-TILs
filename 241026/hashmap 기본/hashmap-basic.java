e0fe2af77a62670d14418f99f2b146e3db110a9e
hchbae <hchbae1001@gmail.com>
2024-10-26 12:44:59 +0900

    Title: hashmap 기본, Time: 1301ms, Memory: 51MB, Status: Passed - CodeTree

--- 241026/hashmap 기본/hashmap-basic.java
+++ 241026/hashmap 기본/hashmap-basic.java
@@ -3,31 +3,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();  // 줄바꿈 문자 제거
-
-        HashMap<Integer, Integer> m = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();  // 명령의 수
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
-            String command = sc.nextLine();
-            String[] arg = command.split(" ");
-            
-            if (arg[0].equals("add")) {
-                int k = Integer.parseInt(arg[1]);
-                int v = Integer.parseInt(arg[2]);
-                m.put(k, v);
-            } else if (arg[0].equals("remove")) {
-                int k = Integer.parseInt(arg[1]);
-                m.remove(k);
-            } else if (arg[0].equals("find")) {
-                int k = Integer.parseInt(arg[1]);
-                if (m.containsKey(k)) {
-                    System.out.println(m.get(k));
-                } else {
-                    System.out.println("None");
-                }
+            String command = scanner.next();  // 명령어
+            int key = scanner.nextInt();      // key값
+
+            switch (command) {
+                case "add":
+                    int value = scanner.nextInt();
+                    map.put(key, value);       // key, value 쌍 추가 (덮어쓰기 가능)
+                    break;
+                
+                case "remove":
+                    map.remove(key);           // key 값 제거
+                    break;
+
+                case "find":
+                    if (map.containsKey(key)) {
+                        result.append(map.get(key)).append("\n");  // 값 출력
+                    } else {
+                        result.append("None\n");   // 없으면 None 출력
+                    }
+                    break;
             }
         }
+        
+        System.out.print(result.toString());
+        scanner.close();
     }
 }
