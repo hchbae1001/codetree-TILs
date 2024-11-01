985de95d098cf3023356b93400a6812963720ccc
hchbae <hchbae1001@gmail.com>
2024-11-01 17:54:48 +0900

    Title: 큰 숫자만 계속 고르기, Time: 638ms, Memory: 30MB, Status: Passed - CodeTree

--- 241101/큰 숫자만 계속 고르기/keep-picking-the-big-number.java
+++ 241101/큰 숫자만 계속 고르기/keep-picking-the-big-number.java
@@ -1,21 +1,25 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         Scanner sc = new Scanner(System.in);
         String[] nm = sc.nextLine().split(" ");
         int n = Integer.parseInt(nm[0]);
         int m = Integer.parseInt(nm[1]);
         String[] arg = sc.nextLine().split(" ");
 
-        for(String s : arg){
-            int tmp = Integer.parseInt(s);
-            pq.add(tmp);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String s : arg) {
+            pq.add(Integer.parseInt(s));
         }
-        for(int i = 0; i < m; i++){
-            int tmp = pq.poll();
-            pq.add(tmp-1);
+
+        while (m-- > 0) {
+            int max = pq.poll();
+            pq.add(max - 1);
         }
-        System.out.println(pq.poll());
+
+        System.out.println(pq.peek());
+        sc.close();
     }
 }
