f2b33945357fe98ebadf6b0d89b38edf12496681
hchbae <hchbae1001@gmail.com>
2024-11-11 00:48:37 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 384ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 241111/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+        
+        for (int i = 1; i < n; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        System.out.println(maxSum);
+    }
+}
